70a995c724cfa31936ce0e163f294d998d7a0465
Reorx <novoreorx@gmail.com>
2013-11-28 19:46:48 +0800

    save to load at home

--- journal/journal.py
+++ journal/journal.py
@@ -5,12 +5,17 @@ import datetime
 from git import Repo
 
 
-repo = Repo('')
+repo = Repo('/home/reorx/workspace/lab/cc')
 
-today_midnight = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-tomorrow_midnight = today_midnight + datetime.timedelta(days=1)
+today_start = datetime.datetime.now().replace(hour=6, minute=0, second=0, microsecond=0)
 
-for i in repo.iter_commits():
-    if i.authored_date < today_midnight:
-        break
-    i.author
+#for i in repo.iter_commits():
+    #if i.authored_date < today_midnight:
+        #break
+    #i.author
+
+print dir(repo)
+print 'on migration'
+for i in repo.iter_commits('migration'):
+    print type(i.author), i.committed_date >= i.authored_date
+print i.author
