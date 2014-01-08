b5e03a23471dc922f6a0d02acd269cfa441add99
Reorx <novoreorx@gmail.com>
2014-01-08 19:23:59 +0800

    write settings view

--- workjournal/static/js/base.js
+++ workjournal/static/js/base.js
@@ -12,9 +12,66 @@
     });
 
     var SettingsView = Backbone.View.extend({
+        events: {
+        },
+
+        show: function() {
+            this.el.show(1000);
+        },
+
+        hide: function() {
+            this.el.hide(1000);
+        }
     });
 
+    var settingsView = new SettingsView({el: $('.settings')});
+
     var EditorView = Backbone.View.extend({
+    });
+
+    var NavView = Backbone.View.extend({
+        activeButton: function(name) {
+            this.$('a').removeClass('active');
+            this.$(name).addClass('active');
+        }
+    });
+
+    navView = new NavView({el: $('.nav')});
+
+
+    var Router = Backbone.Router.extend({
+        routes: {
+            'today': 'today',
+            'oneday': 'oneday',
+            'settings': 'settings'
+        },
+
+        today: function() {
+        },
+
+        oneday: function() {
+        },
+
+        settings: function() {
+            console.log('settings');
+            navView.activeButton('settings');
+            settingsView.$el.show();
+        }
+    })
+
+    router = new Router();
+
+    Backbone.history.start({pushState: true});
+
+    $(document).on('click', 'a:not([data-bypass])', function (e) {
+
+        var href = $(this).attr('href');
+        var protocol = this.protocol + '//';
+
+        if (href.slice(protocol.length) !== protocol) {
+            e.preventDefault();
+            router.navigate(href, true);
+        }
     });
 
 })();
